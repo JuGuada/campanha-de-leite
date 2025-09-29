fed8022ee5baef66bd9d6620b248c32d2c18944e
JuGuada <juliaguadanhim10@gmail.com>
2025-09-29 15:20:58 -0300

    cria projeto doacao de leite

---
+++ script.js
@@ -0,0 +1,23 @@
+const slides = [
+    "/img/amarelo.png",
+    "/img/rosa.png",
+    "/img/azul2.png",
+   
+];
+var indiceAtual = 0;
+
+function mostrarSlide() {
+    var slide = document.getElementById("carrossel-slide");
+    slide.style.backgroundImage = `url(${slides[indiceAtual]})`
+}
+function proximoSlide(){
+    if(indiceAtual === slides.length -1){
+        indiceAtual=0
+    }else{
+        indiceAtual++
+    }
+    mostrarSlide()
+}
+mostrarSlide()
+
+setInterval(proximoSlide, 3000)
